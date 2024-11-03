54b0a0b981a5d94cba456f9262eaec97ac21a30f
Vladimir <18351400+en9inerd@users.noreply.github.com>
2024-11-03 09:46:26 -0500

    Refactor build process to support multi-platform binaries and update Makefile

--- Makefile
+++ Makefile
@@ -1,9 +1,9 @@
 # Variables
 GO=go
 BUILD_DIR=build
-BINARY_NAME=j2z
+DIST_DIR=dist
+BINARY_NAME=$(shell basename $(PWD))
 BINARY_PATH=$(BUILD_DIR)/$(BINARY_NAME)
-CGO_ENABLED=0
 
 # Targets
 all: build
@@ -12,10 +12,11 @@ build:
 	$(GO) build -o $(BINARY_PATH) ./src
 
 build-prod:
-	CGO_ENABLED=$(CGO_ENABLED) $(GO) build -gcflags="all=-l -B" -trimpath -ldflags="-s -w" -o $(BINARY_PATH) ./src
+	bash scripts/build.sh
 
 clean:
 	rm -rf $(BUILD_DIR)
+	rm -rf $(DIST_DIR)
 
 .PHONY: all build clean
 
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
+CGO_ENABLED=0
+DIST_DIR="dist"
+
 platforms=("linux/amd64" "linux/arm64" "darwin/amd64" "darwin/arm64" "windows/amd64" "windows/arm64")
 
 for platform in "${platforms[@]}"
 do
   GOOS="${platform%/*}"
   GOARCH="${platform#*/}"
-  output="dist/$(basename $PWD)_${GOOS}_${GOARCH}"
+  output="$DIST_DIR/$(basename $PWD)_${GOOS}_${GOARCH}"
 
   if [ "$GOOS" == "windows" ]; then
     output="$output.exe"
   fi
 
   echo "Building $output"
-  GOOS=$GOOS GOARCH=$GOARCH go build -gcflags="all=-l -B" -trimpath -ldflags="-s -w" -o $output ./src
+  CGO_ENABLED=$CGO_ENABLED GOOS=$GOOS GOARCH=$GOARCH go build -gcflags="all=-l -B" -trimpath -ldflags="-s -w" -o $output ./src
 done
